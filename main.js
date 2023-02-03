bb9896bb085243661f93236be778d7455f176be8
SNOOZY <100608802+SnOoOzY@users.noreply.github.com>
2023-02-03 14:08:55 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+/* function auth() {
+      var txtEmail = document.getElementById("txtEmail").value;
+       var txtPassword = document.getElementById("txtPassword").value;
+             if(txtEmail == "devcom@gmail.com" && txtPassword == "admin1234") {
+                     window.location.assign("site.html");
+                      alert("Login Success");
+               } else {
+                    alert("Email or Password is Incorrect, Please try Again.")
+                    return;
+             }
+   
+         }
+
+
+function auth2() {
+
+}
+*/
